fbf8137299f9115b38d402b8acca06d975144f3f
frittblas <frittblas@gmail.com>
2023-03-19 19:27:10 +0100

    screening page

--- src/components/ScreeningList.jsx
+++ src/components/ScreeningList.jsx
@@ -3,26 +3,6 @@ import { Link } from 'react-router-dom';
 
 export default function ScreeningList() {
   const s = useStates('main');
-  /*
-    s.movies.map(({ id, title }) => {
-  
-      console.log("movie id and name: ", id, title);
-  
-    });*/
-
-  let allMovies = [];
-  s.movies.map((movie) => {
-
-    console.log("movie id and name: ", movie.id, movie.title, movie.description.posterImage);
-
-    allMovies.push(movie);
-
-  });
-
-  console.log("allMovies ", allMovies);
-
-  //const { title, description } = movie;
-  //const { length, categories, posterImage } = description;
 
   return <>
     {s.screenings.map(({ id, time, movieId, auditoriumId }) => <Link
@@ -38,8 +18,9 @@ export default function ScreeningList() {
           minute: "numeric",
         }).format(new Date(time))}</h2>
 
-        {/*<img src={'https://cinema-rest.nodehill.se' + d.posterImage} />*/}
-        <p>movieId {movieId}</p>
+        {s.movies[movieId - 1] === undefined ? <p>"Loading"</p> :
+          <img src={'https://cinema-rest.nodehill.se' + s.movies[movieId - 1].description.posterImage} />}
+        <p>{s.movies[movieId - 1] === undefined ? "Loading" : s.movies[movieId - 1].title}</p>
         {auditoriumId === 1 ? <p>Lilla salen</p> : <p>Stora salen</p>}
         <hr />
       </div>
